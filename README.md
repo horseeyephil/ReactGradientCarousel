d56f36ef261e7b6d7cae0c63e134101aa06b95ff
Philip Gingerich <philipygingerich@gmail.com>
2018-07-11 15:45:59 -0400

    Created demo and fixed readme

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # React Gradient Carousel 
-This package is for a custom React component button, that plays an attractive gradient shimmer animation upon hover. 
+This package is for a custom React button component that animates an attractive gradient shimmer animation upon hover. 
 
 The button takes an onClick prop as well as props for custom styling.
 
-##### Props (all optional)
-``` 
-onClick : A callback to triggered on the button's click event
-colorTheme: A color if provided to determine the button's border and main gradient theme. Note: 8-digit hex values may be provided to specify the color's alpha (opacity)   value. Named color or rgba value string can also be used.
-gradientCenter: The color to contrast in the center of the gradient
-customStyling: An style object to provide for custom CSS attributes for the button. If not provided, a default setting will be used. Note: a borderColor property may be    specified in order to overwrite the colorTheme prop and provide a border that is different than the gradient theme
-```
+#### Props (all optional)
+
+**onClick** : A callback to triggered on the button's click event
+
+**colorTheme**: A color if provided to determine the button's border and main gradient theme. Note: 8-digit hex values may be provided to specify the color's alpha (opacity) value. Named color or rgba value string can also be used.
+
+**gradientCenter**: The color to contrast in the center of the gradient
+
+**customStyling**: An style object to provide for custom CSS attributes for the button. If not provided, a default setting will be used. Note: a borderColor property may be specified in order to overwrite the colorTheme prop and provide a border that is different than the gradient theme
+
 
 ```javascript
+
+    import GradientCarouselButton from react-gradient-carousel
+
     <GradientCarouselButton
         onClick = {_=>{console.log('This is a click handler!')}} 
         colorTheme = 'paleturquoise' //this is a named color
@@ -21,7 +27,13 @@ customStyling: An style object to provide for custom CSS attributes for the butt
             borderRadius: 20,
             borderStyle: 'dotted'
         }
-
-    >Here is a custom button with all its props!
+        >Here is a custom button and all its props!
     </GradientCarouselButton>
+```
+
+#### To run the demo:
+Clone the repo
+```
+npm i 
+npm run docs
 ```
--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -6,12 +6,12 @@ import "./styles.css";
 function Demo() {
   return (
     <div>
-      <Glow>Here is a demo of my React Component!</Glow>
+      <Glow colorTheme = 'lightblue' gradientCenter = 'steelblue' customStyling = {{fontSize: 50}}>Here is a demo of my React Component!</Glow>
       <Glow>This is a default setting with no props provided.</Glow>
-      <Glow>Here is a 'Rose' theme</Glow>
-      <Glow>This is a button with custom styling provided</Glow>
-      <Glow>Next comes Moby Dick</Glow>
-      <Glow>
+      <Glow colorTheme = 'darkseagreen' gradientCenter = 'pink'>Here is a 'Rose' theme</Glow>
+      <Glow colorTheme = 'mediumvioletred' gradientCenter = 'lightcoral' customStyling = {{font: '20px century, serif', padding: 15, width: 75, borderStyle: 'dotted', display: 'block' }}>This is a button with custom styling provided</Glow>
+      <Glow colorTheme = 'mediumslateblue' gradientCenter = 'bisque' customStyling={{width: 160, display: 'block', margin: 'auto'}}>Next comes Moby Dick</Glow>
+      <Glow colorTheme = 'slategray' gradientCenter = 'thistle'>
       Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off—then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me.
 
 There now is your insular city of the Manhattoes, belted round by wharves as Indian isles by coral reefs—commerce surrounds it with her surf. Right and left, the streets take you waterward. Its extreme downtown is the battery, where that noble mole is washed by waves, and cooled by breezes, which a few hours previous were out of sight of land. Look at the crowds of water-gazers there.
